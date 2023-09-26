d79b35ae56ac328c503cb3b671e9f1301115aa35
ajaymahiwal <ajaymahiwal5@gmail.com>
2023-09-26 19:52:44 +0530

    hashmap

--- Practice/Hashing/Inbuilt.java
+++ Practice/Hashing/Inbuilt.java
@@ -3,5 +3,14 @@ import java.util.*;
 class Inbuilt{
     public static void main(String args[]){
         HashMap<String,Integer> map = new HashMap<>();
+        map.put("India",1947);
+        map.put("UK",1947);
+        map.put("USA",1742);
+
+       Set<String> keys = map.keySet();
+
+       for(String k : keys){
+        System.out.println(k+"->"+map.get(k));
+       } 
     }
 }
